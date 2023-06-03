c16e0b4d3430ac717e080432ca92184f7964a876
smamedov451@gmail.com <smamedov451@gmail.com>
2023-06-04 02:55:55 +0300

    exportCSV

---
+++ FifthTaskItransition/wwwroot/js/downloadCSV.js
@@ -0,0 +1,31 @@
+﻿
+function convertArrayOfObjectsToCSV(args) {
+    var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+    data = args.data;
+    if (data == null || !data.length) {
+        return null;
+    }
+
+    columnDelimiter = args.columnDelimiter || ';';
+    lineDelimiter = args.lineDelimiter || '\r\n';
+
+    keys = Object.keys(data[0]);
+
+    result = '';
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+
+    data.forEach(item => {
+        ctr = 0;
+        keys.forEach(key => {
+            if (ctr > 0) {
+                result += columnDelimiter;
+                result += item[key];
+                ctr++;
+            }
+        });
+        result += lineDelimiter;
+    });
+    return result;
+};
--- FifthTaskItransition/wwwroot/js/site.js
+++ FifthTaskItransition/wwwroot/js/site.js
@@ -36,3 +36,38 @@ slider.onchange = function () {
     Init(Seedtext.value, text.value);
     page = 2;
 }
+
+
+var download = document.getElementById("downloadCSV");
+
+download.onclick = function () {
+
+    var rows = window.document.querySelectorAll("table tr");
+
+    var allColumns = [];
+
+    for (var i = 0; i < rows.length; i++) {
+        var cols = rows[i].querySelectorAll('td');
+        for (var j = 0; j < cols.length; j++) {
+            allColumns.push(cols[j].textContent)
+        }
+    }
+
+    const chunkArray = (arr, cnt) => arr.reduce((prev, cur, i, a) => !(i % cnt) ? prev.concat([a.slice(i, i + cnt)]) : prev, []);
+
+    var CSVData = chunkArray(allColumns, 5);
+     
+
+    let csvContent = "data:text/csv;charset=utf-8,";
+
+    for (var key in CSVData) {
+        CSVData[key].forEach(function (e, i) {
+            CSVData[key][i] = (e || "").replace(/\s*\n\s*/g, "");
+        });
+    } 
+
+    var result = CSVData.join("\n");
+     
+    var encodedUri = encodeURI(csvContent) + '\ufeff' + encodeURI(result);
+    window.open(encodedUri);
+ }
